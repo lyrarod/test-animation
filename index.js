fc2d9a2aa9faf34c8ab8ee8d73eef5de19c9b3e1
lyrarod <lyrarod@gmail.com>
2022-02-16 21:44:41 -0300

    simple spritesheet

---
+++ index.js
@@ -0,0 +1,29 @@
+var animationInterval;
+var spriteSheet = document.getElementById("sprite-image");
+var widthOfSpriteSheet = 1472;
+var widthOfEachSprite = 184;
+
+function stopAnimation() {
+  clearInterval(animationInterval);
+}
+
+function startAnimation() {
+  var position = widthOfEachSprite; //start position for the image
+  const speed = 100; //in millisecond(ms)
+  const diff = widthOfEachSprite; //difference between two sprites
+
+  animationInterval = setInterval(() => {
+    spriteSheet.style.backgroundPosition = `-${position}px 0px`;
+
+    if (position < widthOfSpriteSheet) {
+      position = position + diff;
+    } else {
+      //increment the position by the width of each sprite each time
+      position = widthOfEachSprite;
+    }
+    //reset the position to show first sprite after the last one
+  }, speed);
+}
+
+//Start animation
+startAnimation();
