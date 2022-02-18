66821012a2de1818bc9793d14e03a2254887fd31
lyrarod <lyrarod@gmail.com>
2022-02-18 16:12:05 -0300

    start

---
+++ index.js
@@ -0,0 +1,5 @@
+import { loop, start } from "./loop.js";
+
+window.addEventListener("load", () => {
+  start();
+});
---
+++ loop.js
@@ -0,0 +1,34 @@
+import Sprite from "./sprite.js";
+
+let balls = [];
+
+export const loop = () => {
+  render();
+  update();
+  requestAnimationFrame(loop);
+};
+
+export const start = () => {
+  init();
+  loop();
+};
+
+const init = () => {
+  console.log("init...");
+  let numBalls = 100;
+
+  for (let i = 0; i < numBalls; i++) {
+    balls.push(new Sprite());
+  }
+  console.log(balls);
+};
+
+const update = () => {
+  // console.log("update...");
+  balls.map((ball) => ball.update());
+};
+
+const render = () => {
+  // console.log("render...");
+  balls.map((ball) => ball.render());
+};
